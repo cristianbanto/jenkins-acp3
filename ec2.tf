8f518e580ab62d3224c2be18051e8c707bae6a2f
cristianbanto <86287982+cristianbanto@users.noreply.github.com>
2023-03-14 11:31:08 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region     = "us-east-1"
-  }
+  assume_role {
+    duration_seconds = 3600
+    session_name = "session-name"
+    role_arn = arn:aws:iam::985536073477:role/jenkins-role
+  }  
+}
 
 resource "aws_instance" "myec2test" {
   ami           = "ami-00874d747dde814fa"  
