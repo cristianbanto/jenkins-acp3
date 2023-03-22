27ec147b30a8139087a823cd538541203f5a4d42
cristianbanto <86287982+cristianbanto@users.noreply.github.com>
2023-03-22 12:48:56 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,24 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2test" {
-  ami           = "ami-00874d747dde814fa"  
+  ami           = "ami-0557a15b87f6559cf"  
   instance_type = "t2.micro"
+  
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install openjdk-11-jdk
+  sudo ufw allow 8080
+  sudo cd /tmp
+  wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.87/bin/apache-tomcat-8.5.87.tar.gz
+  tar xzvf apache-tomcat-8.5.87.tar.gz 
+  sudo systemctl start tomcat
+  
+  echo "*** Completed Installing apache2"
+  EOF
+  
+  tags = {
+    Name = "Apache Server"
+  }
 }
